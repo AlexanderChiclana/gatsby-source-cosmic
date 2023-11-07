628b0ae8e381d05ad97672315772ebea48d92e23
Alexander Chiclana <alexander.chiclana@gmail.com>
2023-11-07 13:59:33 -0500

    add log

--- src/helpers/deleteCacheItem.js
+++ src/helpers/deleteCacheItem.js
@@ -2,6 +2,7 @@ import getCacheID from './getCacheID';
 
 const deleteCacheItem = async (nodeAPIHelpers, options, cacheItemSlug) => {
   const { cache } = nodeAPIHelpers;
+  console.log('cache', cache)
   const cacheID = getCacheID(options, cacheItemSlug);
   await cache.del(cacheID);
 };
